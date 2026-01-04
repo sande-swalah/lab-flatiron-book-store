45ac7d3432c0416000ad31bd8f47a643513cb221
sande-swalah <alvinswalah@gmail.com>
2026-01-04 20:58:15 +0300

    DOM done

--- index.js
+++ index.js
@@ -45,3 +45,30 @@ const bookStore = {
 
 // Write your code here!
 
+// Select the header and set the bookstore name
+const bookStoreTitle = document.querySelector('#header')
+bookStoreTitle.textContent = bookStore.name
+
+// Select the book list container
+const bookList = document.querySelector('#book-list')
+
+// Loop through books and create elements
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement('li')
+
+    const bookTitle = document.createElement('h3')
+    bookTitle.textContent = book.title
+
+    const bookAuthor = document.createElement('p')
+    bookAuthor.textContent = book.author
+
+    const bookImage = document.createElement('img')
+    bookImage.src = book.imageUrl
+
+    bookContainer.appendChild(bookTitle)
+    bookContainer.appendChild(bookAuthor)
+    bookContainer.appendChild(bookImage)
+
+    bookList.appendChild(bookContainer)
+})
+
